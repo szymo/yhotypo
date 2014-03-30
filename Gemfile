f121017efcbac912da70c7959a017a8894995141
Szymon Boniecki <szymon@monterail.com>
2014-03-30 13:41:18 +0200

    config changes

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+source 'http://rails-assets.org'
+
+gem "middleman", "~> 3.1.5"
+gem "middleman-syntax"
+gem "nokogiri"
+gem "builder", "~> 3.0.0"
+gem "middleman-deploy", "~> 0.0.12"
+gem "middleman-livereload"
+gem "middleman-minify-html"
--- config.rb
+++ config.rb
@@ -70,11 +70,9 @@
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-  
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_html
+  activate :minify_css
+  activate :minify_javascript
   
   # Enable cache buster
   # activate :cache_buster
